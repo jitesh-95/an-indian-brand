51efcb59f74542407693d756c330b4f3d71df031
jitesh-95 <1995sirohis@gmail.com>
2022-09-04 02:48:54 +0530

    filter contidions done in routes

--- backend/controllers/kids.controller.js
+++ backend/controllers/kids.controller.js
@@ -3,22 +3,84 @@ const ProductsModel = require("../models/Products.model");
 const kidsRouter = express.Router();
 
 kidsRouter.get("/kids", async (req, res) => {
-  const { page } = req.query;
-  // console.log(page);
+  const { page, sortBy } = req.query;
   const limit = 20;
   const skip = (+page - 1) * limit;
 
-  const kidsProducts = await ProductsModel.find({ category: "kids" })
-    .skip(skip)
-    .limit(limit);
-  const totalKidsProducts = await ProductsModel.find({ category: "kids" });
-  if (kidsProducts.length > 0) {
-    return res.send({
-      kidsProducts,
-      total: totalKidsProducts.length,
-      response: true,
-    });
+  if (sortBy == "null") {
+    const kidsProducts = await ProductsModel.find({ category: "kids" })
+      .skip(skip)
+      .limit(limit);
+    const totalKidsProducts = await ProductsModel.find({ category: "kids" });
+    if (kidsProducts.length > 0) {
+      return res.send({
+        kidsProducts,
+        total: totalKidsProducts.length,
+        response: true,
+      });
+    }
   }
+
+  if (sortBy === "HighToLow") {
+    const kidsProducts = await ProductsModel.find({ category: "kids" })
+      .skip(skip)
+      .limit(limit)
+      .sort({ price: -1 });
+    const totalKidsProducts = await ProductsModel.find({ category: "kids" });
+    if (kidsProducts.length > 0) {
+      return res.send({
+        kidsProducts,
+        total: totalKidsProducts.length,
+        response: true,
+      });
+    }
+  }
+
+  if (sortBy === "LowToHigh") {
+    const kidsProducts = await ProductsModel.find({ category: "kids" })
+      .skip(skip)
+      .limit(limit)
+      .sort({ price: 1 });
+    const totalKidsProducts = await ProductsModel.find({ category: "kids" });
+    if (kidsProducts.length > 0) {
+      return res.send({
+        kidsProducts,
+        total: totalKidsProducts.length,
+        response: true,
+      });
+    }
+  }
+
+  if (sortBy === "A-Z") {
+    const kidsProducts = await ProductsModel.find({ category: "kids" })
+      .skip(skip)
+      .limit(limit)
+      .sort({ name: 1 });
+    const totalKidsProducts = await ProductsModel.find({ category: "kids" });
+    if (kidsProducts.length > 0) {
+      return res.send({
+        kidsProducts,
+        total: totalKidsProducts.length,
+        response: true,
+      });
+    }
+  }
+
+  if (sortBy === "Z-A") {
+    const kidsProducts = await ProductsModel.find({ category: "kids" })
+      .skip(skip)
+      .limit(limit)
+      .sort({ name: -1 });
+    const totalKidsProducts = await ProductsModel.find({ category: "kids" });
+    if (kidsProducts.length > 0) {
+      return res.send({
+        kidsProducts,
+        total: totalKidsProducts.length,
+        response: true,
+      });
+    }
+  }
+
   res.send({ message: "Error", response: false });
 });
 
--- backend/controllers/men.controller.js
+++ backend/controllers/men.controller.js
@@ -3,21 +3,84 @@ const ProductsModel = require("../models/Products.model");
 const menRouter = express.Router();
 
 menRouter.get("/men", async (req, res) => {
-  const { page } = req.query;
-  // console.log(page);
+  const { page, sortBy } = req.query;
   const limit = 20;
   const skip = (+page - 1) * limit;
-  const mensProducts = await ProductsModel.find({ category: "men" })
-    .skip(skip)
-    .limit(limit);
-  const totalMensProducts = await ProductsModel.find({ category: "men" });
-  if (mensProducts.length > 0) {
-    return res.send({
-      mensProducts,
-      total: totalMensProducts.length,
-      response: true,
-    });
+
+  if (sortBy == "null") {
+    const mensProducts = await ProductsModel.find({ category: "men" })
+      .skip(skip)
+      .limit(limit);
+    const totalMensProducts = await ProductsModel.find({ category: "men" });
+    if (mensProducts.length > 0) {
+      return res.send({
+        mensProducts,
+        total: totalMensProducts.length,
+        response: true,
+      });
+    }
   }
+
+  if (sortBy === "HighToLow") {
+    const mensProducts = await ProductsModel.find({ category: "men" })
+      .skip(skip)
+      .limit(limit)
+      .sort({ price: -1 });
+    const totalMensProducts = await ProductsModel.find({ category: "men" });
+    if (mensProducts.length > 0) {
+      return res.send({
+        mensProducts,
+        total: totalMensProducts.length,
+        response: true,
+      });
+    }
+  }
+
+  if (sortBy === "LowToHigh") {
+    const mensProducts = await ProductsModel.find({ category: "men" })
+      .skip(skip)
+      .limit(limit)
+      .sort({ price: 1 });
+    const totalMensProducts = await ProductsModel.find({ category: "men" });
+    if (mensProducts.length > 0) {
+      return res.send({
+        mensProducts,
+        total: totalMensProducts.length,
+        response: true,
+      });
+    }
+  }
+
+  if (sortBy === "A-Z") {
+    const mensProducts = await ProductsModel.find({ category: "men" })
+      .skip(skip)
+      .limit(limit)
+      .sort({ name: 1 });
+    const totalMensProducts = await ProductsModel.find({ category: "men" });
+    if (mensProducts.length > 0) {
+      return res.send({
+        mensProducts,
+        total: totalMensProducts.length,
+        response: true,
+      });
+    }
+  }
+
+  if (sortBy === "Z-A") {
+    const mensProducts = await ProductsModel.find({ category: "men" })
+      .skip(skip)
+      .limit(limit)
+      .sort({ name: -1 });
+    const totalMensProducts = await ProductsModel.find({ category: "men" });
+    if (mensProducts.length > 0) {
+      return res.send({
+        mensProducts,
+        total: totalMensProducts.length,
+        response: true,
+      });
+    }
+  }
+
   res.send({ message: "Error", response: false });
 });
 
